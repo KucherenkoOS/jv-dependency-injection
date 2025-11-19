e80765182f5c289503ec29b511cc16ead581a5ab
Olexander Kucherenko <amarokthegrey@gmail.com>
2025-11-19 14:44:38 +0200

    checkstyle fix

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,25 +1,25 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
 import mate.academy.service.ProductService;
 import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Injector {
     private static final Injector injector = new Injector();
-
-    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     private static final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
             FileReaderService.class, FileReaderServiceImpl.class,
             ProductParser.class, ProductParserImpl.class,
             ProductService.class, ProductServiceImpl.class
     );
+
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     private Injector() {
     }
